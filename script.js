380a08d356f224ed1ef678846b9d181c764b96e2
zq405 <yuhan.bai.06@gmail.com>
2026-02-01 04:26:59 +0800

    BookList not finish yet

---
+++ script.js
@@ -0,0 +1,35 @@
+function Login()
+{
+    alert("Login successful!");
+    window.location.href()="dashboard.html";
+}
+
+function addToCart(book)
+{
+    alert(book + " added to cart!");
+}
+
+function paynow()
+{
+    alert("Payment Successful!");
+}
+
+function submitComment()
+{
+    alert("Thank you for your feedback!");
+}
+
+const books=
+[
+    {
+        tittle:"The Digital Revolution",
+        author:"John Revolution",
+        category:"science & technology",
+        price:"$9.99",
+        link:"book1.html"
+    },
+    {
+        tittle:"Beyond the Horizon",
+
+    }
+]
